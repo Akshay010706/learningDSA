2df0844316e886cf1fa24d8a07e7c53ad5bf5ac0
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-08 16:45:56 +0530

    practce and more methods

--- Array/StartingAlgorithm/jugglingalogrithm.py
+++ Array/StartingAlgorithm/jugglingalogrithm.py
@@ -7,33 +7,70 @@
 
 # M1. dublicate list 
 # time - o(n) space -o(n)
+# def rotate(arr,d):
+#     n= len(arr)
+#     new = [0]*n
+#     for i in range(n):
+#         new[(i+d)%n] = arr[i]
+#     return new    
+
+
+
+
+
 #m2 - repeated rotation 
 # time o(n*d)  space - o(n)
+# def rotate(arr,d):
+#     n = len(arr)
+#     for j in range(d):
+#         last = arr[-1]
+    
+
+#     for i in range(n-1,0,-1):
+#         arr[i] = arr[i-1]
+
+#     arr[0] = last
+#     return arr
+
+
+
+
+
 #m3 - juggling algo -- time -o(n)   space-o(1)
 
-#Juggling algorithm 
+#Juggling algorithm
 # swapping in cycled 
 # gcd(n,d%n)
+def gcd_val(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
-import math
-
-def rotate(arr,d):
-    n = len(arr)
-    gcdval = math.gcd(n,d%n)
-    for i in range(gcdval):
-        temp = arr[i]
-        j=i
-        while True:
-            k = (j-d)%n
-            if k == i:
-                break
-            arr[j] = arr[k]
-            j=k
+# def rotate(arr,d):
+#     n = len(arr)
+#     #number of cylcle is find by gcd
+#     gcdval = gcd_val(n,d%n)
+#     for i in range(gcdval):
+#         temp = arr[i]
+#         j=i
+#         while True:
+#             k = (j+d)%n# left rotation 
+#             # for right rotation k = (j - d + n) % n 
+#             #when one cycle is completed , we arrived at from where we started we break the loop
+#             #0-2-4-0 break loop
+#             if k == i:
+#                 break
+#             arr[j] = arr[k]
+#             j=k
             
-        arr[j] = temp
+#         arr[j] = temp
         
-    return arr
-   
+#     return arr
+
+
+
+
+
 arr = [1,2,3,4,5]
 print(rotate(arr,2))     
         
