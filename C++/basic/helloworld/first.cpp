182d3a9d63b446b3d2cb9f96a9a18b501add8f21
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-23 20:22:09 +0530

    c++ basic and variables type

---
+++ C++/basic/helloworld/first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//to not use std:: everytime we can use 
+//using namespace std;
+
+int main() {
+    std::cout<<"Hello World!"<<std::endl;
+    //endl goes to next line
+    // or cout<<endl; or \n
+    //but if not used it will be printed in the same line
+    std::cout<<"akshay";
+}
