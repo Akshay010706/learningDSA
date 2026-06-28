2f47ed298a387de7450c5cb3bae4322e54d06ebd
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-28 16:09:37 +0530

    ternary operator

--- C++/01_Fundamentals/03_Conditionals/if_else_statements.cpp
+++ C++/01_Fundamentals/03_Conditionals/if_else_statements.cpp
@@ -20,6 +20,21 @@ int main() {
         cout<<"you will be 18 in few years";
 
 
+    //ternary operator
+    //Condition ? Expresion1: Expresion2;
+    18>16?cout<<"Akshay":cout<<"Varun"; 
+    
+    int num =9;
+    num%2?cout<<"ODD":cout<<"EVEN";
+    // 1 correspont to teue and 0 to false
+
+    // in boolean 
+    bool hungery = true;
+    hungery?cout<<"i am hungery ":cout<<"i am not hungery";
+    //or
+    cout<<hungery?"yes":"No";
+
+
     }
     
     
