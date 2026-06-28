a125403d327c1b65dd35918b01b5b8f016c189b4
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-28 15:23:41 +0530

    cmath library in C++

---
+++ C++/brocodeC++/basic/mth.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a=3.14;
+    double b =2;
+    double c;
+
+    // c = max(a,b);
+    // c = min(a,b);
+    // c  = pow(2,4);---- 16
+    // c = sqrt(9);
+    // c = abs(-9)
+    //round off
+    // c = round(a);-- int number 
+    //c = ceil(a);-- uper bound
+    // c = floor(a);--- lower bound
+
+
+    
+    return 0;
+}
