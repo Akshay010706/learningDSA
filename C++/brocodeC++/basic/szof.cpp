3e900aecb67090bbce1c64f08b6e8acd2c1c877e
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-28 20:22:41 +0530

    sizeof operator in C++

---
+++ C++/brocodeC++/basic/szof.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //sizeof() - determines the size in bytes of a variable , datatype, class, object etc.
+    double gpa;
+    cout<<sizeof(gpa);
+    return 0;
+}
