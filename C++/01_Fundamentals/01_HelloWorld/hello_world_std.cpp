580e213e0e7e5dfa0d06a92d07142fd63376044a
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-28 15:23:29 +0530

    getline in c++

--- C++/01_Fundamentals/01_HelloWorld/hello_world_std.cpp
+++ C++/01_Fundamentals/01_HelloWorld/hello_world_std.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    cout<<"Hello World!\n";
+    string x;
+    cout<<"Hello World!";
+    //getlene takes input which has white spaces 
+    getline(cin,x);
+     
+    // getline(cin>>ws,x)
+    //ws ignores any newline characters and white spaces
 }
