43361a06b5a2470760c8ada9ab8c99d9a87757b6
Akshay010706 <akshaypratapsingh529@gmail.com>
2026-06-28 19:42:31 +0530

    global and local variable

--- C++/02_Functions/function_overloading.cpp
+++ C++/02_Functions/function_overloading.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+//global variable
+int x = 1;
+
 // Function overloading - same function name, different parameters
 int add(int a, int b) {
     return a + b;
@@ -19,6 +22,9 @@ int main() {
     cout << add(5, 3) << endl;           // calls int version
     cout << add(5.5, 3.2) << endl;       // calls double version
     cout << add(5, 3, 2) << endl;        // calls int, int, int version
+    int x = 6;
+    cout<<x; // 9
+    cout<<::x;//1
     
     return 0;
 }
